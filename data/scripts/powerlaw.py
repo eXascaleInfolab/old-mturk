c4978b04ef5e85c07101a4a787f5f4df4fbc1996
Djellel Eddine Difallah <djelleleddine.difallah@exascale.info>
2015-03-04 18:59:40 +0100

    new graphs

--- data/scripts/powerlaw.py
+++ data/scripts/powerlaw.py
@@ -6,26 +6,18 @@ from collections import defaultdict
 import os
 import sys
 
-dist = {'2009': defaultdict(int),
-    '2010': defaultdict(int),
-    '2011': defaultdict(int),
-    '2012': defaultdict(int),
-    '2013': defaultdict(int),
-    '2014': defaultdict(int)
-}
+dist = defaultdict(int)
 
 def main():
   with open('../raw/all_batch_sizes.tsv') as f:
     next(f)
     for line in f:
-      timestamp, hits_available, hit_group = line.strip().split('\t')
-      year = timestamp[6:]
-      dist[year][hits_available] += 1
+    	timestamp, hits_available, hit_group = line.strip().split('\t')
+    	dist[hits_available] += 1
 
 
 if __name__ == '__main__':
   main()
-  for year, values in dist.items():
-    for hits_available, num in values.items():
-      print year + ',' + hits_available + ',' + str(num)
+  for hits_available, num in dist.items():
+    print hits_available + ',' + str(num)
 
--- data/scripts/powerlaw.r
+++ data/scripts/powerlaw.r
@@ -1,19 +1,21 @@
 library(ggplot2)
 library(reshape2)
 library(scales)
-data <- read.csv(file = '/Users/pirroh/exascale-mturk/data/raw/powerlaw.csv', header=T, sep=',')
+library(MASS)
+data <- read.csv(file = '/Users/xi/repositories/exascale-mturk/data/raw/powerlaw.csv', header=T, sep=',')
 attach(data)          
 
-ggplot(data, aes(x = batch_size, y = instances, fill=year)) +
-  geom_point(alpha=1/3, colour='blue') +
-  facet_wrap(~ year, ncol=2, scales="fixed") +
-  scale_colour_brewer(palette="Set1") +
-  ylab("# of Batches") +
-  xlab("Batch Size") +
-  scale_x_log10() +
-  scale_y_log10() +
-  theme_bw() +
+ggplot(data, aes(x = batch_size, y = instances)) +
+  geom_point(alpha=1/3, colour='#0072B2') +
+  scale_colour_brewer() +
+  ylab("Number of Batches (log)") +
+  xlab("Batch Size (log)") +
+  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                   labels = trans_format("log10", math_format(10^.x))) +
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                   labels = trans_format("log10", math_format(10^.x)))+
+  theme_bw() + annotation_logticks() +
   theme(legend.position="none", axis.text.x = element_text(angle = 90, hjust = 1, vjust =0.5))
 
-ggsave("/Users/pirroh/exascale-mturk/paper/figures/powerlaw.pdf", width=7, height=8)
+ggsave("/Users/xi/repositories/exascale-mturk/paper/figures/powerlaw.pdf", width=6, height=4)
 
