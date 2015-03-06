366fd3d3d8f31be5d1c055a369136f06841fdb03
Michele Catasta <pirroh@fooshed.net>
2015-03-06 20:05:20 +0100

    smaller throughput plot

--- data/scripts/plot_throughput.r
+++ data/scripts/plot_throughput.r
@@ -1,7 +1,7 @@
 library(scales)
 library(ggplot2)
 data <- read.csv(file = '/Users/pirroh/exascale-mturk/data/raw/throughput_5cent_price_3months_final.txt', header=T, sep='\t')
-data <- data[-sample(1:1915573, 1715573, replace=F), ]
+data <- data[-sample(1:1915573, 1815573, replace=F), ]
 attach(data)
 p <- ggplot(data.frame(x=hits, y=throughput), aes(x = x, y = y)) +  
    geom_point(size=1,alpha=1/3, colour='#0072B2') + 
@@ -13,6 +13,6 @@ p <- ggplot(data.frame(x=hits, y=throughput), aes(x = x, y = y)) +
                 labels = trans_format("log10", math_format(10^.x))) +
    scale_y_continuous(breaks = trans_breaks("log10", function(x) 10^x),
                      labels = trans_format("log10", math_format(10^.x))) +
-   xlab("#hits_available (log)") + ylab("Throughput HITs/Minute (log)") + 
+   xlab("HIT_available (log)") + ylab("Throughput HITs/Minute (log)") + 
    theme_bw() + theme(legend.position="none")
 ggsave("motiv_mturk.pdf", width=6, height=4)
